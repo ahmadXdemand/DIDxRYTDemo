08c5713966b0e1a9c0a0cdc9f56b43bc880dfab4
ahmadXdemand <ahmad.shahzaib@xdemand.ai>
2025-04-28 10:17:19 -0700

    added

---
+++ src/utils/pinataService.ts
@@ -0,0 +1,6 @@
+const PINATA_API_KEY = process.env.PINATA_API_KEY;
+const PINATA_API_SECRET = process.env.PINATA_API_SECRET;
+
+if (!PINATA_API_KEY || !PINATA_API_SECRET) {
+  throw new Error('Pinata API credentials are not defined in environment variables');
+} 
